b5b86a4a04a0e02987baa4a0a3bcfd4fa11fc199
Taiyi <god830311@gmail.com>
2020-08-31 02:50:03 +0800

    insertion sort

---
+++ Algorithms/Sort/insertionSort.js
@@ -0,0 +1,18 @@
+function InsertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    j = i;
+    while (j > 0 && arr[j - 1] > arr[j]) {
+      let tmp = arr[j];
+      arr[j] = arr[j - 1];
+      arr[j - 1] = tmp;
+      j -= 1;
+    }
+  }
+}
+
+const arr = Array(10)
+  .fill()
+  .map(() => Math.round(Math.random() * 100));
+console.log("before: ", arr);
+InsertionSort(arr);
+console.log("after:  ", arr);
