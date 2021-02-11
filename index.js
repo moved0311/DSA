3d90799d5131ef5800a1bff5500fb83748272bc2
Taiyi <taiyi@aptan.io>
2021-02-11 22:47:20 +0800

    merge sort

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-import bubbleSort from "./Algorithms/Sort/bubbleSort";
+import mergeSort from "./Algorithms/Sort/mergeSort";
 const arrs = [
-  [3, 2, 1],
-  [4, 2, 6, 1, 0, 3, 9],
-  [10, 4, 9, 7, 0, 1, 5],
+  [4, 2, 1, 8, 3, 7, 6, 5],
   [1, 2, 3, 4, 5, 6, 7],
+  [-10, 2, 11, 5, 6, 40],
+  [-22, -33, 41, 14, 56],
 ];
 
 arrs.map((arr) => {
   console.log("input: ", arr);
-  console.log("output: ", bubbleSort(arr));
+  console.log("output: ", mergeSort(arr));
   console.log("===========================");
 });
